7a2255108c238bcb41fccc0bf7146f12af75a280
TienPH25783 <tienppph25783@fpt.edu.vn>
2022-11-20 16:47:40 +0700

    projcet HirBerChucVu

---
+++ CuaHangDienThoai/src/main/java/DomainModel/ChucVu.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DomainModel;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.bytebuddy.implementation.bind.annotation.Empty;
+
+/**
+ *
+ * @author ongbi
+ */
+    @Table(name = "ChucVu")
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    @Entity
+public class ChucVu {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "Id")
+   private UUID id;
+   
+   @Column(name = "Ma", unique = true)
+   private String ma;
+   
+   @Column(name = "Ten",columnDefinition = "nvarchar(Max)")
+   private String ten;
+   
+   @Column(name = "Luong")
+   private BigDecimal Luong;
+   
+}
